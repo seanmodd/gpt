9fec1f67a4d0891b4696a91db090ba1a44752781
Sean <seansmodd@gmail.com>
2023-01-29 18:29:07 -0800

    first

--- gpt.py
+++ gpt.py
@@ -1,2 +1,20 @@
+import openai
+
+API_KEY = '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'
 
 
+openai.api_key = API_KEY
+
+model = 'text-davinci-003'
+
+response = openai.Completion.create(
+  prompt='How big is the moon',  
+  model=model,
+     max_tokens=5,
+  temperature=0.9,
+  n=3, 
+  stop=['\n', ' Human:', ' AI:']
+  
+)
+
+print(response.usage.total_tokens)
